517b7330181ed89c2038e3242205daeab1260928
Svmvyv <Samayanader@gmail.com>
2022-03-16 05:13:40 +0300

    caraousel for popular movies

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
-fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=c485dd249a7a925a38936033459731d5&language=en-US")
-    .then(response => response.json())
-    .then(data => console.log(data.genres))
-    .catch(err => {
-        console.error(err);
-    });
+const API_KEY = 'api_key=c485dd249a7a925a38936033459731d5';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
+
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        showMovies(data.results);
+
+    })
+
+}
+
+
+function showMovies(data) {
+
+    data.forEach(movie => {
+        const { title, poster_path, vote_average, overview, id } = movie;
+        const moviecard = document.querySelector('carousel-inner');
+        // moviecard.classList.add('movie');
+        moviecard.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span>${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Know More</button
+            </div>
+        
+        `
+
+    })
+}

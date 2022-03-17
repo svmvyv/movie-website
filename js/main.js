14c9f8bf9bd502cc8fe7454facafe7e659e45661
Svmvyv <Samayanader@gmail.com>
2022-03-17 18:49:24 +0300

    finishing touches

--- js/main.js
+++ js/main.js
@@ -9,16 +9,16 @@ axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
         document.getElementById("trending-list").innerHTML = response.data.results.map(Item =>
             `   <div class="movie-card">
         <img src="${IMG_URL+Item.poster_path}" alt="movie poster" class="movie-poster">
-        <div class="movie-info">
-
+        <div class="movie-info d-flex">
             <button type="button" class="btn-look btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Details
               </button>
+             <div class="text-white p-1 float-end">
+             <i class="bi bi-bookmark"></i>
+             <i class="bi bi-heart"></i>
+          </div>
         </div>
-        <div class="container-fluid p-3 justify-content-center">
-            <i class="fa-solid fa-bookmark"></i>
-            <i class="fa-solid fa-heart-circle-plus"></i>
-        </div>
+      
 
 
     </div>`
